f1107ed655916daf1fc8aa8b8ad2c65151b52f8d
Iwan Aucamp <aucampia@gmail.com>
2018-04-25 22:01:43 +0200

    updated

--- README.rst
+++ README.rst
@@ -1,42 +1,24 @@
-DNSPod Intl. API support
+xadix DNSPod Bindings and CLI
 ================================================================================
 
+This is bindings and cli for the dnspod intl api as defined here https://www.dnspod.com/docs/index.html
 
-Commands
+CLI Usage
 --------------------------------------------------------------------------------
+
+Tool installs as ``xdx-dnspod``.
+
+* Credentials can be provides with 
+** Command line arguments ``--email EMAIL`` and ``--password PASSWORD``
+** Environment variables ``XADIX_DNSPOD_EMAIL=EMAIL`` and ``XADIX_DNSPOD_PASSWORD=PASSWORD``
+** JSON Configuration file (which can be set with ``--config CONFIG``, defaults to ``~/.config/xadix-dnspod/config.json``) as ``{"email": "EMAIL", "password": "PASSWORD"}``
+* Alternatively, a token (obtained with ``xdx-dnspod auth``) can be used. This can be provided with:
+** Command line arguments ``--token TOKEN``
+** Environment variables ``XADIX_DNSPOD_TOKEN=TOKEN``
+** JSON Cache file (which can be set with ``--cache CACHE``, defaults to ``~/.config/xadix-dnspod/cache.json``) as ``{"token": "TOKEN"}``
+
+Example usage:
 
 ::
-    # run ...
-    python -m xadix.dnspod.cli --version
-
-    # versioneer ...
-    pip install --user --upgrade versioneer
-    versioneer install
-    python2 setup.py version
-
-    # view docs ...
-    pip2 install -v --user --upgrade --ignore-installed restview
-    restview README.rst
-
-    # install ...
-    pip2 install --user --upgrade .
-    pip3 install --user --upgrade .
-    
-    mog-cli
-    mog-main
-    
-    pip2 install --user --upgrade --editable .
-    pip3 install --user --upgrade --editable .
-
-    # ...
-    package = x/__init__.py
-    module = x.py
-
-    # ....
-    vimdiff <(curl --silent https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore) .gitignore
-
-References
---------------------------------------------------------------------------------
-
-* https://www.dnspod.com/docs/index.html
-* https://github.com/DNSPod/dnspod-int-api-docs
+    xdx-dnspod domain record -d example.com upsert -n x0 -t A -v 127.1.2.3 -x 60
+    xdx-dnspod domain record -d example.com list
--- xadix/dnspod/cli.py
+++ xadix/dnspod/cli.py
@@ -14,175 +14,7 @@ import json
 import os.path
 #import . as dnspod
 from . import *
-
-'''
-class DnspodException(Exception):
-    def __init__(self, message, data):
-        self._message = message
-        self.data = data
-
-    def __str__(self):
-        return self._message
-
-class Records(object):
-
-    def __init__(self,base,domain_id):
-        self.base = base
-        self.domain_id = domain_id
-
-    def list(self):
-        result = self.base._dispatch("Record.List", {"domain_id": self.domain_id})
-        return { "domain": result["domain"], "info": result["info"], "records": result["records"] }
-    
-    def create(self, **kwargs):
-        query = { "domain_id": self.domain_id }
-        query["sub_domain"] = kwargs["name"]
-        query["record_type"] = kwargs["type"]
-        query["value"] = kwargs["value"]
-        query["record_line"] = kwargs["line"]
-        if "mx_priority" in kwargs and kwargs["mx_priority"] is not None: query["mx"] = kwargs["mx_priority"]
-        if "ttl" in kwargs and kwargs["ttl"] is not None: query["ttl"] = kwargs["ttl"]
-        result = self.base._dispatch("Record.Create", query)
-        return result
-
-    def get_id(self, **kwargs):
-        result = self.list()
-        for record in result["records"]:
-            if record["name"] == kwargs["name"] and record["line"].lower() == kwargs["line"].lower() and record["type"] == kwargs["type"]: return record["id"]
-        return None
-    
-    def get(self, **kwargs):
-        result = self.list()
-        for record in result["records"]:
-            if record["name"] == kwargs["name"] and record["line"].lower() == kwargs["line"].lower() and record["type"] == kwargs["type"]: return record
-        return None
-
-    def remove(self, **kwargs):
-        if "record_id" not in kwargs:
-            record_id = self.get_id(**kwargs)
-        else:
-            record_id = kwargs["record_id"]
-        result = self.base._dispatch("Record.Remove", { "domain_id": self.domain_id, "record_id": record_id })
-        return result
-
-    def modify(self, **kwargs):
-        if "record_id" not in kwargs:
-            lookup = { "name": kwargs["match_name"], "type": kwargs["match_type"], "line": kwargs["match_line"] }
-            record_id = self.get_id(**lookup)
-        else:
-            record_id = kwargs["record_id"]
-        query = { "domain_id": self.domain_id, "record_id": record_id }
-        query["sub_domain"] = kwargs["name"]
-        query["record_type"] = kwargs["type"]
-        query["value"] = kwargs["value"]
-        query["record_line"] = kwargs["line"]
-        if "mx_priority" in kwargs and kwargs["mx_priority"] is not None: query["mx"] = kwargs["mx_priority"]
-        if "ttl" in kwargs and kwargs["ttl"] is not None: query["ttl"] = kwargs["ttl"]
-        result = self.base._dispatch("Record.Modify", query)
-        return result
-
-    def upsert(self, **kwargs):
-        record_id = self.get_id(**kwargs)
-        if record_id is not None:
-            query = kwargs.copy()
-            query["record_id"] = record_id
-            result = self.modify(**query)
-        else:
-            query = kwargs.copy()
-            result = self.create(**query)
-        return result
-
-class Domains(object):
-
-    def __init__(self,base):
-        self.base = base
-
-    def remove(self, **kwargs):
-        if "domain_name" in kwargs:
-            domain_id = self.get_id(kwargs["domain_name"])
-        else:
-            domain_id = kwargs["domain_id"]
-        result = self.base._dispatch("Domain.Remove", { "domain_id": domain_id })
-
-    def create(self, domain_name):
-        result = self.base._dispatch("Domain.Create", { "domain": domain_name })
-        return result["domain"]
-
-    def get_id(self, domain_name):
-        result = self.list()
-        for domain in result["domains"]:
-            if domain["name"] == domain_name: return domain["id"]
-        return None
-
-    def get(self, domain_name):
-        result = self.list()
-        for domain in result["domains"]:
-            if domain["name"] == domain_name: return domain
-        return None
-
-    def list(self):
-        result = self.base._dispatch("Domain.List", {})
-        return { "info": result["info"], "domains": result["domains"] }
-
-    def records(self, **kwargs):
-        if "domain_name" in kwargs:
-            domain_id = self.get_id(kwargs["domain_name"])
-        else:
-            domain_id = kwargs["domain_id"]
-        return Records(self.base, domain_id)
-
-class User(object):
-    def __init__(self,base):
-        self.base = base
-
-    def detail(self):
-        result = self.base._dispatch("User.Detail", {})
-        return result["info"]
-
-class Base(object):
-    def __init__(self, **kwargs):
-        self.url = kwargs.get("url", 'https://api.dnspod.com')
-        self.urlp = urlparse.urlparse(self.url)
-        self.domains = Domains(self)
-        self.user = User(self)
-        self.user_token = None
-
-    def _dispatch(self, path, iquery):
-        query = iquery.copy()
-        query["format"] = "json"
-        if self.user_token is not None and "user_token" not in query:
-            query["user_token"] = self.user_token
-        query_string = urllib.urlencode(query)
-        #query_string = "login_email={:s}&login_password={:s}&format=json".format( kwargs.get("email"), kwargs.get("password") )
-        url = urlparse.urlunparse(
-            ( self.urlp.scheme, self.urlp.netloc, posixpath.join( self.urlp.path, path ),
-            self.urlp.params, query_string, self.urlp.fragment ) )
-        logging.debug("url = %s, query_string = %s", url, query_string)
-        response = requests.post(url, query_string, headers={ "Content-Type": "application/x-www-form-urlencoded" })
-        logging.debug("response = %s", response)
-        logging.debug("response.headers = %s", response.headers)
-        logging.debug("response.text = %s", response.text)
-        response_data = response.json()
-        logging.debug("response_data = %s", response_data)
-        if ( response_data["status"]["code"] != "1" ):
-            raise DnspodException("response.text = {}".format(response.text), response_data)
-        return response_data
-
-    def auth(self, **kwargs):
-        query = { "login_email": kwargs.get("email"), "login_password": kwargs.get("password") }
-        response_data = self._dispatch("Auth", query)
-        self.user_token = response_data["user_token"]
-        return self.user_token
-
-    def check_token(self, token):
-        try:
-            result = self._dispatch("User.Detail", {"user_token": token})
-        except DnspodException as e:
-            if e.data["status"]["code"] != "1": return False
-        return True
-        
-
-'''
+from . import __version__
 
 def format_dlist(dlist, fmt):
     if fmt=="json":
@@ -203,7 +35,6 @@ try:
 except ImportError:
     # Python 2
     import httplib as http_client
-
 
 def do_auth(base, arguments):
     cache_data = {}   
@@ -338,47 +169,6 @@ def main():
     domain_record_upsert_subparser.add_argument("-v", "--value", action="store", dest="record_value", type=str, required=True, help="...")
     domain_record_upsert_subparser.add_argument("-m", "--mx-priority", action="store", dest="record_mx_priority", type=str, required=False, help="...")
     domain_record_upsert_subparser.add_argument("-x", "--ttl", action="store", dest="record_ttl", type=str, required=False, help="...")
-
-    '''
-    argument_subparsers = argument_parser.add_subparsers(dest="subparser0", help="...")
-    if True:
-        argument_subparser = argument_subparsers.add_parser("auth")
-        argument_subparser.add_argument("-e", "--email", action="store", dest="email", type=str, required=True, help="...")
-        argument_subparser.add_argument("-p", "--password", action="store", dest="password", type=str, required=True, help="...")
-    if True:
-        argument_subparser = argument_subparsers.add_parser("user")
-        argument_subparsers = argument_subparser.add_subparsers(dest="subparser1", help="...")
-        if True:
-            argument_subparser = argument_subparsers.add_parser("detail")
-    if True:
-        argument_subparser = argument_subparsers.add_parser("domain")
-        argument_subparsers = argument_subparser.add_subparsers(dest="subparser1", help="...")
-        if True:
-            argument_subparser = argument_subparsers.add_parser("list")
-        if True:
-            argument_subparser = argument_subparsers.add_parser("create")
-            argument_subparser.add_argument("-d", "--domain-name", action="store", dest="domain_name", type=str, required=True, help="...")
-            #argument_subparser.add_argument("domain_name", action="store", type=str)
-        if True:
-            argument_subparser = argument_subparsers.add_parser("remove")
-            argument_subparser.add_argument("-d", "--domain-name", action="store", dest="domain_name", type=str, required=True, help="...")
-            #argument_subparser.add_argument("domain_name", action="store", type=str)
-        if True:
-            argument_subparser = argument_subparsers.add_parser("record")
-            argument_subparser.add_argument("-d", "--domain-name", action="store", dest="domain_name", type=str, required=True, help="...")
-            argument_subparsers = argument_subparser.add_subparsers(dest="subparser2", help="...")
-            if True:
-                argument_subparser = argument_subparsers.add_parser("list")
-            if True:
-                argument_subparser = argument_subparsers.add_parser("create")
-                argument_subparser.add_argument("-s", "--name", action="store", dest="record_name", type=str, required=True, help="...")
-                argument_subparser.add_argument("-t", "--type", action="store", dest="record_type", type=str, required=True, help="...")
-                argument_subparser.add_argument("-l", "--line", action="store", dest="record_line", type=str, required=False, default="Default", help="...")
-                argument_subparser.add_argument("-v", "--value", action="store", dest="record_value", type=str, required=True, help="...")
-                argument_subparser.add_argument("-m", "--mx-priority", action="store", dest="record_mx_priority", type=str, required=False, help="...")
-                argument_subparser.add_argument("-T", "--ttl", action="store", dest="record_ttl", type=str, required=False, help="...")
-    '''     
-        
     arguments = root_parser.parse_args( args = sys.argv[1:] )
 
     if arguments.verbosity is not None:
@@ -398,12 +188,6 @@ def main():
 
     #do_auth(base)
     if arguments.subparser0 != "auth":
-        '''
-        if arguments.token is None:
-            base.auth( email = arguments.email, password = arguments.password )
-        else:
-            base.user_token = arguments.token
-        '''
         do_auth(base, arguments)
 
     if False: None
